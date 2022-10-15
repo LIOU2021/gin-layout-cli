3a9c93d81e013ac7c17b13f82d6ff3bba7109030
LIOU <aa78789898tw@gmail.com>
2022-10-16 03:04:50 +0800

    add oprtion

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # gin-layout-cli
 - https://github.com/LIOU2021/gin-layouts
+
+# introduction
+Install the cobra generator with the command go install github.com/LIOU2021/gin-layout-cli@latest. Go will automatically install it in your $GOPATH/bin directory which should be in your $PATH.
+
+# ref
+- https://github.com/spf13/cobra/blob/main/user_guide.md
--- cmd/create.go
+++ cmd/create.go
@@ -10,31 +10,32 @@ import (
 )
 
 // createCmd represents the create command
-var createCmd = &cobra.Command{
-	Use:   "create [file type]",
-	Short: "create some file",
-	Long: `A longer description that spans multiple lines and likely contains examples
+var (
+	// Used for flags.
+	fileName string
+
+	createCmd = &cobra.Command{
+		Use:   "create [file type]",
+		Short: "create some file",
+		Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(args)
-		fmt.Println("create called")
-	},
-}
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("args : ", args)
+			fmt.Println("file name is : ", fileName)
+			fmt.Println("create called")
+		},
+	}
+)
 
 func init() {
 	rootCmd.AddCommand(createCmd)
 
 	// Here you will define your flags and configuration settings.
 
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// createCmd.PersistentFlags().String("foo", "", "A help for foo")
+	rootCmd.PersistentFlags().StringVarP(&fileName, "name", "n", "", "name")
 
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// createCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
